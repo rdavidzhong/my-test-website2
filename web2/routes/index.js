b141c47ef4ac90fb581895caba0f7dfac4b6d696
codename <daction-z@hotmail.com>
2015-12-26 01:20:48 -0800

    Sync

--- web2/routes/index.js
+++ web2/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+// Add appdata in routes index from Lesson 26
+var appdata = require('../data.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var myArtwork = [];
+  appdata.speakers.forEach(function(item) {
+    myArtwork = myArtwork.concat(item.artwork);
+  });
+  res.render('index', {
+    title: 'Home',
+    artwork: myArtwork
+  });
 });
 
 module.exports = router;
